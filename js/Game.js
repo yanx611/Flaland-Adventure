fdafd35d7cbf9ca48f3039ce5f030b1dc6a1d355
yanx611 <yanx611@gmail.com>
2016-04-09 21:17:39 -0400

    some randomness to shapes and speeds

--- js/Game.js
+++ js/Game.js
@@ -24,7 +24,7 @@ class mainChar
 		this.moveSpeed=5;
 		addToRedraw(this);
 	}
-	
+
 	updateVals(rotationSign, positionSign)
 	{
 		this.rot+=this.rotSpeed*rotationSign;
@@ -43,14 +43,14 @@ class fallingShape
 		var vertex= [[]]; 	//1st spot is which vertex, second is x or y
 		for(var i=0; i<this.sides;i++)
 		{
-			if (!vertex[i]) 
+			if (!vertex[i])
 			{
 				vertex[i] = [];
 			}
 			vertex[i][this.X]=centerX+this.radius*Math.cos((Math.PI/180)*(angle*i+rot));
 			vertex[i][this.Y]=centerY+this.radius*Math.sin((Math.PI/180)*(angle*i+rot));
 		}
-		
+
 		this.ctx.beginPath();
 		this.ctx.moveTo(vertex[0][this.X],vertex[0][this.Y]);
 		for(var i=1; i< this.sides; i++)
@@ -61,7 +61,7 @@ class fallingShape
 		this.ctx.closePath();
 		this.ctx.fill();
 	}
-	
+
 	redraw(canvas)
 	{
 		this.regularPolygon(this.Xpos,this.Ypos,this.rotation);
@@ -71,9 +71,9 @@ class fallingShape
 			this.rotation-360;
 		if(this.Ypos>canvas.height+this.radius)
 			this.Ypos=-this.radius;
-		
+
 	}
-	
+
 	constructor(sides, ctx, radius, startX, startY, rotSpeed, fallSpeed, color)
 	{
 		this.sides=sides;
@@ -104,7 +104,7 @@ function addToRedraw(Shape)
 	allShapes.push(Shape);
 }
 
-	
+
 function keyPress()
 {
 	var e=e||event;
@@ -131,7 +131,7 @@ function keyPress()
 			break;
 		}
 		case 38: //up
-		case 40: //down 
+		case 40: //down
 		default:
 		{
 			break;
@@ -143,12 +143,31 @@ function keyPress()
 function onloadHandler(){
 	var c=document.getElementById("myCanvas");
 	var ctx=c.getContext("2d");
+	//by create random number
+	var shape_line = [0,0,0,0,0];
+	var radius = [0,0,0,0,0];
+	var rotspeeds = [0, 0, 0, 0, 0];
+	var fallspeeds = [0, 0, 0, 0, 0];
+	var i = 0;
+	for (i = 0; i < 5; ++i ) {
+		shape_line[i] = Math.floor(Math.random() * (10)) + 1;
+		radius[i] = Math.floor(Math.random() * (50 - 25 + 1)) + 25;
+		rotspeeds[i] = Math.floor(Math.random() * (4)) + 1;
+		fallspeeds[i] = Math.floor(Math.random() * (4)) + 1;
+	}
+	new fallingShape(shape_line[0], ctx, radius[0], 60, 0, rotspeeds[0], fallspeeds[0], "#FFC200");
+	new fallingShape(shape_line[1], ctx, radius[1], 180, 0, rotspeeds[1], fallspeeds[1], "#FF5B00");
+	new fallingShape(shape_line[2], ctx, radius[2], 300, 0, rotspeeds[2], fallspeeds[2], "#B80028");
+	new fallingShape(shape_line[3], ctx, radius[3], 420, 0, rotspeeds[3], fallspeeds[3], "#84002E");
+	new fallingShape(shape_line[4], ctx, radius[4], 540, 0, rotspeeds[4], fallspeeds[4], "#4AC0F2");
+/*
 	new fallingShape(5, ctx, 50, 60, 30, 1, 1, "#FFC200");
 	new fallingShape(4, ctx, 25, 200, 75, 2, 2, "#FF5B00");
 	new fallingShape(3, ctx, 25, 260, 25, 3, 3, "#B80028");
 	new fallingShape(6, ctx, 25, 320, 25, 4, 4, "#84002E");
 	new fallingShape(12, ctx, 25, 400, 75, 5, 5, "#4AC0F2");
+*/
 	var intervalID= setInterval(redrawAll, 10,c,ctx);
 	mainCharacter= new mainChar(c,ctx);
-	
+
 }
