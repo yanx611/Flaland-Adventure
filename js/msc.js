e10ecc86ba2ffbe98746a7e7f5734985337ae308
glennd2rpi <glennd2rpi@gmail.com>
2016-05-08 23:23:49 -0400

    Added files via upload

--- js/msc.js
+++ js/msc.js
@@ -1,9 +1,40 @@
 
 
 var dark = document.createElement('audio');   
+var cossack = document.createElement('audio');
+var kazanka = document.createElement('audio');
+cossack.setAttribute('src','snd/cossack.wav');
 dark.setAttribute('src', 'snd/8bit.wav');
-//dark.play();
+kazanka.setAttribute('src','snd/kazanka.wav');
+
+function darkeyes()
+
+{
+kazanka.pause();
+cossack.pause();
+dark.play();
 dark.loop=true;
+}
 
-
-        
+function cossacks ()
+{ 
+dark.pause();
+kazanka.pause();      
+cossack.play();
+cossack.loop=true;
+ }
+ 
+ function kaza
+ {
+ dark.pause();
+ cossack.pause();
+ kazanka.play();
+ kazanka.loop=true;
+ }
+ function muteall()
+ 
+ {
+ dark.pause();
+ cossack.pause();
+ kazanka.pause();
+ }
